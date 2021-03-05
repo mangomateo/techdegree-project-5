d4031ed5c1bf1e2aab4cf7256e206fb8a8f11b3f
mangomateo <velarde.matthew@gmail.com>
2021-03-05 11:56:04 -0800

    Generate 12 random employees

--- js/script.js
+++ js/script.js
@@ -2,26 +2,38 @@ const gallery = document.querySelector('#gallery');
 
 
 /**
- * Generate HTML for employee cards
+ * Fetch random user data from API
  **/
-const generateEmployeeCards = () => {
+async function getEmployeeData(url) {
+    const employees = await fetch(url);
+    const employeesJSON = await employees.json();
+    console.log(employeesJSON);
+
+    generateEmployeeCards(employeesJSON.results);
+    
+}
+
+getEmployeeData('https://randomuser.me/api/?results=12&nat=ca');
+
+/**
+ * Generate HTML for employee cards and append to DOM
+ **/
+const generateEmployeeCards = data => {
     let employeeCardHTML = '';
     
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < data.length; i++) {
         employeeCardHTML += `
         <div class="card">
             <div class="card-img-container">
-                <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+                <img class="card-img" src="${ data[i].picture.large }" alt="profile picture">
             </div>
             <div class="card-info-container">
-                <h3 id="name" class="card-name cap">first last</h3>
-                <p class="card-text">email</p>
-                <p class="card-text cap">city, state</p>
+                <h3 id="name" class="card-name cap">${ data[i].name.first } ${ data[i].name.last }</h3>
+                <p class="card-text">${ data[i].email }</p>
+                <p class="card-text cap">${ data[i].location.city }, ${ data[i].location.state }</p>
             </div>
         </div>`;
     }
     
     gallery.insertAdjacentHTML('beforeend', employeeCardHTML);
 }
-
-generateEmployeeCards();
